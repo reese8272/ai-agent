651c1152f071e34911c332bb909bd71c2807e6cf
reese8272 <reesepludwick@gmail.com>
2025-06-08 18:55:15 -0400

    main reconfigurations, no more response method

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
-
-def respond(client, contents):
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=contents
-    )
-    print(response.text)
-    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
-    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     
-
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMENI_API_KEY")
     client = genai.Client(api_key = api_key)
+
+    if len(sys.argv) < 2:
+        print("Invalid argument. Please type: python3 main.py '{your_prompt}'")
+        sys.exit(1)
+
+    verbose = False
+    if "--verbose" in sys.argv:
+        verbose = True
+
     prompt = sys.argv[1]
-    respond(client, prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
+
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+
+    if verbose:
+        print(f"User prompt: {prompt}")
+    print(response.text)
+    if verbose:
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+        print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
 
 if __name__ == "__main__":
     main()
