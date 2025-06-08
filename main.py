ed17b43c45e14262288466908126702bedcd2640
reese8272 <reesepludwick@gmail.com>
2025-06-08 18:23:30 -0400

    updated gitinore, successfully insantiated the model

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+def respond(client, contents):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=contents
+    )
+    print(response.text)
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+    
+
+
 def main():
-    print("Successful Commit")
+    load_dotenv()
+    api_key = os.environ.get("GEMENI_API_KEY")
+    client = genai.Client(api_key = api_key)
+    prompt = sys.argv[1]
+    respond(client, prompt)
 
 if __name__ == "__main__":
     main()
