824c1c0eb9d8a57f6f95591027dedb2bf6adf4e0
reese8272 <reesepludwick@gmail.com>
2025-06-10 17:58:53 -0400

    finalized write file and updated tests.py

--- functions/write_file.py
+++ functions/write_file.py
@@ -7,10 +7,10 @@ def write_file(working_directory, file_path, content):
         target_file = os.path.abspath(os.path.join(working_directory,file_path))
         if not target_file.startswith(abs_working_dir):
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-        if not os.path.exists(target_file):
-            os.makedirs(target_file)
+        if os.path.dirname(target_file):
+                os.makedirs(os.path.dirname(target_file), exist_ok=True)
         with open(target_file, 'w') as f:
             f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
-        return f'{e}'
+        return f'Error: {e}'
--- tests.py
+++ tests.py
@@ -1,8 +1,21 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
-    
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result for calc, lorem.txt, wait, this isn't...")
+    print(result)
+    print("")
+
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result for calc, pkg/morelorem.txt, lorem ipsum dolor...")
+    print(result)
+    print("")
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result for calc, tmp/temp.txt, this should not be allowed")
+    print(result)
 
 
 
