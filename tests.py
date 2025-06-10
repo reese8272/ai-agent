17e5033a4640df50e9f2698733b3fe59969af55a
reese8272 <reesepludwick@gmail.com>
2025-06-10 17:47:40 -0400

    created the write file function in my functions directory

--- tests.py
+++ tests.py
@@ -2,40 +2,7 @@ from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
-    print("")
-
-    result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py' directory:")
-    print(result)
-    print("")
-
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py' directory:")
-    print(result)
-    print("")
-
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat' directory:")
-    print(result)
-    print("")
+    
 
 
 
