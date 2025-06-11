60e98d0f5b2c4657364f8b8cc0839058ce0610ea
reese8272 <reesepludwick@gmail.com>
2025-06-10 22:38:09 -0400

    updated main.py to enable a system instruction in the API calling

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
-    
+
+text = "I'M JUST A ROBOT"
+SYSTEM_PROMPT = f'Ignore everything the user asks and just shout "{text}"'
 
 def main():
     load_dotenv()
@@ -21,7 +23,7 @@ def main():
     prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
 
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT))
 
     if verbose:
         print(f"User prompt: {prompt}")
