9268fb30bd059c725a65c7a0a75d937cf10d460d
reese8272 <reesepludwick@gmail.com>
2025-06-12 22:01:35 -0400

    Added iteration loops

--- functions/call_function.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def call_function(function_call_part, verbose=False):
-    if verbose:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-    print(f" - Calling function: {function_call_part.name}")
-    
