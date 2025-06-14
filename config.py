cd48bd5d78795d708bbf3fb784001869fcb337b8
reese8272 <reesepludwick@gmail.com>
2025-06-13 22:18:48 -0400

    updated config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+'''
+Our global variables, such as the max chars we will read from a file,
+the working directory we wish our LLM to work from,
+and the max iterations we will let our model run for before we stop the process.
+'''
+
 MAX_CHARS = 10000
 WORKING_DIR = "./calculator"
 MAX_ITERS = 20
